53181108a252b5f53956fc386c34930d67fb59b8
Liam McKay <lmckay@msg.ucsf.edu>
2020-09-28 14:50:44 -0700

    added arguments to main

--- run_update_bearbuy_inventory.py
+++ run_update_bearbuy_inventory.py
@@ -3,11 +3,18 @@ import bearbuy_scrape
 import get_delivery_information
 import logistics_info_scrape
 import merge_logistics_delivery_requisition
-
+import argparse
 
 def main():
-    # updateReq_selenium.main()
-    bearbuy_scrape.main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-reqtsv", "--requisition_directory", help="optional path to location of HTML files",
+                        required=False)
+    parser.add_argument("-file", "--file_extension", help="optional file extension for HTM files", required=False)
+    parser.add_argument("-email", "--email_find", help="optional search gmail for purchase orders", required=False, action='store_true')
+    parser.add_argument("-r", "--hard_run", help="overwrite allRequisitions.tsv", required=False, action='store_true')
+    args = parser.parse_args()
+    updateReq_selenium.main(args)
+    bearbuy_scrape.main(args)
     # get_delivery_information.main()
     # logistics_info_scrape.main()
     # merge_logistics_delivery_requisition.main()
