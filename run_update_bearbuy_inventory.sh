2c35acfb75a0e885fed5970927aa6331a0ab67fe
Liam McKay <35275985+liammckay19@users.noreply.github.com>
2020-09-29 14:17:53 -0700

    Delete run_update_bearbuy_inventory.sh

--- run_update_bearbuy_inventory.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source /Users/liam_msg/Documents/automation_local/automation_env/bin/activate
-python3 /Users/liam_msg/Documents/automation/updateReq_selenium.py
-python3 /Users/liam_msg/Documents/automation/bearbuy_scrape.py
-open /Users/liam_msg/Documents/automation/allRequisitions.tsv
-python3 /Users/liam_msg/Documents/automation/get_delivery_information.py -reqtsv /Users/liam_msg/Documents/automation/allRequisitions.tsv
-python3 /Users/liam_msg/Documents/automation/logistics_info_scrape.py
-open /Users/liam_msg/Documents/automation/logistics_info_scrape_output.tsv
